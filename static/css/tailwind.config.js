b1f91038d18f51f1c80e8eb176066c147d812bdf
Monal-Patel <monal-@hotmail.com>
2022-12-13 13:11:35 +0000

    about page complete

--- static/css/tailwind.config.js
+++ static/css/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["../../layouts/**/*.{html,js}"],
+  content: ["../../layouts/**/*.{html,js}","../../../../post/**/*.md"],
   theme: {
     extend: {},
     fontFamily: {'tufte': ['et-book','serif']
